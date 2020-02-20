fa407c384303ab6e65976b50986d25a64470de3e
Daniel <d.ruizb.1999@gmail.com>
2020-02-20 19:10:12 +0100

    completed the functionality of the product page

--- backend/src/main/java/store/main/controller/ComponentController.java
+++ backend/src/main/java/store/main/controller/ComponentController.java
@@ -13,8 +13,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import store.main.database.Post;
-import store.main.database.PostRepository;
 import store.main.database.*;
 
 @Controller
@@ -23,10 +21,13 @@ public class ComponentController {
 	private List<Post> postList1 = new LinkedList<Post>();// store posts with user tag1
 	private List<Post> postList2 = new LinkedList<Post>();// store posts with user tag2
 	private List<Post> postList3 = new LinkedList<Post>();// store posts with user tag3
-	private List<Post> finalList= new LinkedList<>();
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RatingRepository ratingRepository;
+
 
 	@Autowired
 	private PostRepository postRepository;
@@ -34,8 +35,8 @@ public class ComponentController {
 	@Autowired
 	private Cart cart;
 
-	@RequestMapping("/post/{id}")
-	public String mapPost(Model model, @PathVariable Long id, HttpSession session, HttpServletRequest request) {
+	@RequestMapping("/post/{id}-{img}")
+	public String mapPost(Model model, @PathVariable Long id, @PathVariable int img, HttpSession session, HttpServletRequest request) {
 
 		cart.cartInit(session);
 
@@ -49,11 +50,12 @@ public class ComponentController {
 
 		Post post = postRepository.findById(id).get();
 		boolean alreadyOn = cartAux.contains(post);
+		post.getnImg();
 
 		model.addAttribute("alreadyOn", alreadyOn);
 		model.addAttribute("post", post);
 
-		// reset lists
+		//reset lists
 		this.postList1 = new LinkedList<>();
 		this.postList2 = new LinkedList<>();
 		this.postList3 = new LinkedList<>();
@@ -63,19 +65,38 @@ public class ComponentController {
 																						// recommendations of a
 																						// registered user
 		} else {
-			loadRecommendationsIntoSession(postList1, postList2, postList3, session, post);// private method that loads
+			loadRecommendationsIntoSesion(postList1, postList2, postList3, session, post);// private method that loads
 																							// the recommendations of a
 																							// visiting user
 		}
-		finalList=new LinkedList<>();
-		
-		addToRecomendedList(postList1,postList2,postList3,finalList,model);
-		
+		LinkedList<Integer> images = new LinkedList<>();
+		for (int i=0; i<post.getnImg(); i++) {
+			images.add(i);
+		}
+		int totalr=0;
+		String seller = post.getUser().getFirstName()+" "+post.getUser().getLastName();
+		model.addAttribute("emptyfeatures", post.getFeatures().matches(".*[a-zA-Z]+.*"));
+		model.addAttribute("userid", post.getUser().getId());
+		model.addAttribute("tag", post.getComponentTag());
+		model.addAttribute("tagname", post.getComponent());
+		model.addAttribute("seller", seller);
+		model.addAttribute("stars", getratings(post, totalr));
+		model.addAttribute("totalrates", countratings(post));
+		model.addAttribute("bname", post.getBrand().getName());
+		model.addAttribute("images", images);
+		model.addAttribute("image", img);
+		model.addAttribute("list1", postList1);
+		model.addAttribute("list2", postList2);
+		model.addAttribute("list3", postList3);
+		model.addAttribute("recomend", (postList1.size() + postList2.size() + postList3.size()) != 0); // there are
+																										// recommended
+																										// posts
+
 		return "shop-single-electronics";
 	}
 
-	@RequestMapping("/post/{id}/removeItem-{idRemove}")
-	public String mapPost(Model model, @PathVariable Long id, @PathVariable Long idRemove, HttpSession session) {
+	@RequestMapping("/post/{id}-{img}/removeItem-{idRemove}")
+	public String mapPost(Model model, @PathVariable Long id, @PathVariable int img, @PathVariable Long idRemove, HttpSession session) {
 
 		Post post = postRepository.findById(id).get();
 		model.addAttribute("post", post);
@@ -91,13 +112,34 @@ public class ComponentController {
 		model.addAttribute("total", total);
 		model.addAttribute("empty", empty);
 
-		addToRecomendedList(postList1,postList2,postList3,finalList,model);
+		LinkedList<Integer> images = new LinkedList<>();
+		for (int i=0; i<post.getnImg(); i++) {
+			images.add(i);
+		}
+		int totalr=0;
+		String seller = post.getUser().getFirstName()+" "+post.getUser().getLastName();
+		model.addAttribute("emptyfeatures", post.getFeatures().matches(".*[a-zA-Z]+.*"));
+		model.addAttribute("userid", post.getUser().getId());
+		model.addAttribute("tag", post.getComponentTag());
+		model.addAttribute("tagname", post.getComponent());
+		model.addAttribute("seller", seller);
+		model.addAttribute("stars", getratings(post, totalr));
+		model.addAttribute("totalrates", countratings(post));
+		model.addAttribute("bname", post.getBrand().getName());
+		model.addAttribute("images", images);
+		model.addAttribute("image", img);
+		model.addAttribute("list1", postList1);
+		model.addAttribute("list2", postList2);
+		model.addAttribute("list3", postList3);
+		model.addAttribute("recomend", (postList1.size() + postList2.size() + postList3.size()) != 0); // there are
+																										// recommended
+																										// posts
 
 		return "shop-single-electronics";
 	}
 
-	@RequestMapping("/post/{id}/itemAdded")
-	public String mapPostCart(Model model, @PathVariable Long id, HttpSession session) {
+	@RequestMapping("/post/{id}-{img}/itemAdded")
+	public String mapPostCart(Model model, @PathVariable Long id, @PathVariable int img, HttpSession session) {
 
 		Post post = postRepository.findById(id).get();
 		model.addAttribute("post", post);
@@ -113,11 +155,32 @@ public class ComponentController {
 		model.addAttribute("total", total);
 		model.addAttribute("empty", empty);
 		
-		
-		addToRecomendedList(postList1,postList2,postList3,finalList,model);
+		LinkedList<Integer> images = new LinkedList<>();
+		for (int i=0; i<post.getnImg(); i++) {
+			images.add(i);
+		}
+		int totalr=0;
+		String seller = post.getUser().getFirstName()+" "+post.getUser().getLastName();
+		model.addAttribute("emptyfeatures", post.getFeatures().matches(".*[a-zA-Z]+.*"));
+		model.addAttribute("userid", post.getUser().getId());
+		model.addAttribute("tag", post.getComponentTag());
+		model.addAttribute("tagname", post.getComponent());
+		model.addAttribute("seller", seller);
+		model.addAttribute("stars", getratings(post, totalr));
+		model.addAttribute("totalrates", countratings(post));
+		model.addAttribute("bname", post.getBrand().getName());
+		model.addAttribute("images", images);
+		model.addAttribute("image", img);
+		model.addAttribute("list1", postList1);
+		model.addAttribute("list2", postList2);
+		model.addAttribute("list3", postList3);
+		model.addAttribute("recomend", (postList1.size() + postList2.size() + postList3.size()) != 0); // there are
+																										// recommended
+																										// posts
 
 		return "shop-single-electronics";
 	}
+
 
 	private void loadRecommendationsIntoBD(List<Post> postList1, List<Post> postList2, List<Post> postList3,
 			HttpServletRequest request, Post post) {
@@ -128,7 +191,7 @@ public class ComponentController {
 		userRepository.save(user); // update user tag list
 	}
 
-	private void loadRecommendationsIntoSession(List<Post> postList1, List<Post> postList2, List<Post> postList3,
+	private void loadRecommendationsIntoSesion(List<Post> postList1, List<Post> postList2, List<Post> postList3,
 			HttpSession session, Post post) {
 		List<String> userTags = (List<String>) session.getAttribute("tags");
 		List<String> list = loadList(userTags, post, postList1, postList2, postList3);
@@ -207,16 +270,43 @@ public class ComponentController {
 			}
 		}
 		return userTags;
+
 	}
 	
-	private void addToRecomendedList(List<Post> postList12, List<Post> postList22, List<Post> postList32,
-			List<Post> finalList2,Model model) {
-		finalList.addAll(postList2);
-		finalList.addAll(postList1);
-		finalList.addAll(postList3);
-		model.addAttribute("recomend", !finalList.isEmpty()); // there are recommended posts
-		model.addAttribute("list", finalList);
+	private List<Boolean> getratings (Post post, int totalr) {
+		List<List<Rating>> rating = new LinkedList<List<Rating>>();
+		for(int i=0;i<6;i++){
+			rating.add((ratingRepository.findBySellerEmailIgnoreCaseAndStars(post.getUser().getEmail(), i)));
+			}
+		int cont =0;
+		for(int i=0;i<rating.size();i++){
+			cont+=rating.get(i).size()*i;
+			totalr+= rating.get(i).size();
+			}
+		if (totalr!=0) {
+		cont= cont / totalr;
+		}
+		List<Boolean>ratingf=new LinkedList<Boolean>();
+		for(int i=0;i<5;i++){
+			if (i<cont) {
+			ratingf.add(true);
+			}
+			else {
+			ratingf.add(false);
+			}
+		}
+		return ratingf;
 	}
 	
-	
+	private int countratings (Post post) {
+		List<List<Rating>> rating = new LinkedList<List<Rating>>();
+		for(int i=0;i<6;i++){
+			rating.add((ratingRepository.findBySellerEmailIgnoreCaseAndStars(post.getUser().getEmail(), i)));
+			}
+		int totalr=0;
+		for(int i=0;i<rating.size();i++){
+			totalr+= rating.get(i).size();
+			}
+		return totalr;
+	}
 }
