ac76feddb45ddd9cadfcbadb20afe91b2de6fc91
Daniel <d.ruizb.1999@gmail.com>
2020-03-03 16:47:49 +0100

    added brand GET method to the REST service

--- backend/src/main/java/store/main/api/BrandRestController.java
+++ backend/src/main/java/store/main/api/BrandRestController.java
@@ -1,10 +1,13 @@
 package store.main.api;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import store.main.api.BrandRestController.completeBrand;
 import store.main.database.Brand;
 import store.main.database.BrandRepository;
 import store.main.database.Post;
@@ -23,19 +27,33 @@ public class BrandRestController {
 	
 	@Autowired
 	BrandRepository brandRepository;
-
-	@GetMapping("/")
-	@JsonView(Brand.BasicInfo.class)
-	public List<Brand> getBrands() {
-		return brandRepository.findAll();
-	}
 	
+	interface completeBrand extends Brand.BasicInfo,Brand.PostsInfo,Post.BasicInfo {}
+
 	@PostMapping("/")
 	@ResponseStatus(HttpStatus.CREATED)
 	@JsonView(Brand.BasicInfo.class)
 	public Brand postBrand(@RequestBody Brand brand) {
 		brandRepository.save(brand);
 		return brand;
+	}
+	
+	@GetMapping("/")
+	@JsonView(completeBrand.class)
+	public Collection<Brand> getBrands() {
+		return brandRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	@JsonView(completeBrand.class)
+	public ResponseEntity<Brand> getBrandId(@PathVariable long id) {
+		Brand brand=brandRepository.findById(id).get();
+		if(brand!=null) {
+			return new ResponseEntity<Brand>(brand,HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<Brand>(HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	
