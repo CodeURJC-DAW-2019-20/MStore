44dd4f5dd2aa42b04527943aacf6fca87850580e
Daniel <d.ruizb.1999@gmail.com>
2020-03-03 17:02:29 +0100

    added rating GET method to REST API

--- backend/src/main/java/store/main/api/RatingRestController.java
+++ backend/src/main/java/store/main/api/RatingRestController.java
@@ -1,11 +1,15 @@
 package store.main.api;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
 
+import store.main.api.RatingRestController.RatingUsers;
 import store.main.database.Rating;
 import store.main.database.RatingRepository;
 import store.main.database.User;
@@ -29,15 +34,27 @@ public class RatingRestController {
 	@Autowired
 	private RatingRepository ratingRepository;
 
-	interface RatingUsers extends Rating.BasicInfo, Rating.BuyerInfo, Rating.SellerInfo, User.BasicInfo {
-		
-	}
+	interface RatingUsers extends Rating.BasicInfo, Rating.BuyerInfo, Rating.SellerInfo, User.BasicInfo {}
 	
-	@JsonView(RatingUsers.class)
+	
 	@GetMapping("/")
-	public List<Rating> getRating(){
+	@JsonView(RatingUsers.class)
+	public Collection<Rating> getBrands() {
 		return ratingRepository.findAll();
 	}
+	
+	@GetMapping("/{id}")
+	@JsonView(RatingUsers.class)
+	public ResponseEntity<Rating> getBrandId(@PathVariable long id) {
+		Optional<Rating> rating=ratingRepository.findById(id);
+		if(rating.isPresent()) {
+			return new ResponseEntity<Rating>(rating.get(),HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<Rating>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
 	@JsonView(RatingUsers.class)
 	@PostMapping("/")
 	@ResponseStatus(HttpStatus.CREATED)
