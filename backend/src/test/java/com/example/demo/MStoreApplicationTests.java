bcab072ac815a8aa4d85341aa84218d4eb5e6b46
Dimonra13 <diegomontoto4@hotmail.com>
2020-03-08 22:59:13 +0100

    Pom updated for jar building

--- backend/src/test/java/com/example/demo/MStoreApplicationTests.java
+++ backend/src/test/java/com/example/demo/MStoreApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MStoreApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
