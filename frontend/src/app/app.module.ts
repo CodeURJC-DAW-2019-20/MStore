322e389e9465719c05be988602b9d3d13da4f6f9
Jose Luis <jlsierrabenito@gmail.com>
2020-04-08 22:26:48 +0200

    Implemented mailchimp

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HttpClientJsonpModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { TagInputModule } from 'ngx-chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -75,6 +75,7 @@ import { LoaderInterceptor } from './services/loader.interceptor';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
+    HttpClientJsonpModule,
     NgbModule,
     IconsModule,
     TagInputModule,
--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Brand } from '../../models/brand.model';
 import { BrandService } from '../../services/brand.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+interface MailChimpResponse {
+  result: string;
+  msg: string;
+}
 
 @Component({
   selector: 'app-footer',
@@ -14,7 +20,9 @@ export class FooterComponent implements OnInit {
   @Output()
   filterPosts = new EventEmitter<{id: number, search: string}>();
 
-  constructor(private brandService: BrandService) { }
+  constructor(
+    private brandService: BrandService,
+    private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getBrands();
@@ -31,6 +39,28 @@ export class FooterComponent implements OnInit {
 
   changePosts(idPost: number, searchPost: string) {
     this.filterPosts.emit({id: idPost, search: searchPost});
+  }
+
+  onSubmit(email: string) {
+    let urlStart = "https://urjc.us4.list-manage.com/subscribe/post-json?u=7816e91fc0e80c209c0a7750e&amp;id=a3e1cc1eb7&";
+
+    const params = new HttpParams()
+    .set('EMAIL', email);
+    //.set('b_c7103e2c981361a6639545bd5_29ca296126', ''); // hidden input name
+
+    const url = urlStart + params.toString();
+
+    this.http.jsonp<MailChimpResponse>(url, 'c').subscribe(
+      response => { 
+        if (response.result === 'error' 
+          && confirm(email + ' is already subscribed to our newsletter. Accept to update your profile')) {
+            location.href = 'https://urjc.us4.list-manage.com/subscribe/send-email?e=bHVuc3NzZXNAZ21haWwuY29t&u=7816e91fc0e80c209c0a7750e&id=a3e1cc1eb7';
+        } else if (response.result !== 'error') {
+          alert(response.msg);
+        }
+      },
+      error => console.log(error)
+    );
   }
 
 }
