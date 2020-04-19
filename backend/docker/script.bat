101970c8e0cff832347a3a6581fc089f37d1a3b1
nmartinezs2017 <48331165+nmartinezs2017@users.noreply.github.com>
2020-04-19 18:19:28 +0200

    Update script.bat

--- backend/docker/script.bat
+++ backend/docker/script.bat
@@ -1,4 +1,6 @@
-docker run --rm -v {{path}}:/usr/src/project -w /usr/src/project maven:alpine mvn package
+docker run --rm --name angular -v {path}/MStore/frontend:/frontend -w /frontend node /bin/bash -c "npm install -g @angular/cli; npm install --no-bin-links; ng build --prod --base-href /new/"
+
+docker run --rm -v {path}/MStore:/usr/src/proyect -w /usr/src/proyect/backend maven:alpine mvn package
 
 cd ..
 
